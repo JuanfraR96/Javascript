f3712ae4f035bdc84bc15582c1de5529b0b17e9f
JuanfraR96 <juanfraruiz1996@gmail.com>
2023-01-24 20:18:31 -0300

    preentrega 2

--- js/main.js
+++ js/main.js
@@ -1,39 +1,91 @@
-   
-      function CalcularIMC(altura, peso) 
-      {
-        altura = Math.round(altura) / 100;
-        peso = Math.round(peso);
-        
-        let indice = (peso / Math.pow(altura, 2));
-        let resultado = "";
-            if (indice < 21)
-            {
-              resultado = "Peso inferior al normal.";
-            }
-            else if (indice >= 21 && indice < 25)
-            {
-              resultado = "Peso Normal.";
-            }
-            else if (indice >= 25 && indice < 30)
-            {
-              resultado = "Peso superior al normal.";
-            }
-            else
-            {
-              resultado = "Obesidad";
-            }		   
-        
-        return "Su IMC es: " + indice.toFixed(2) + " y su clasificación es: " + resultado;
-      }
- 
-      function MostrarResultadoIMC (altura, peso, indice)
-      {
-        let mensaje = "Usted mide " + altura / 100 + " metros y pesa " + peso + " Kg. \n \n  " + indice;
-        alert (mensaje);
-      }	
- 
-      let altura = parseInt(prompt("Indique su altura en centímetros:", ""));
-      let peso = parseInt(prompt("Indique su peso en kilógramos:", ""));
- 
-      let resultado = CalcularIMC(altura, peso);
-      MostrarResultadoIMC(altura, peso, resultado); 
+let cantidad_facturas = Number(prompt("ingrese cantidad de Facturas")) //PIDO LA CANTIDAD PARA EL CICLO DE FOR
+let total = 0
+let promedio = 0
+let numero_factura = 0
+
+const SALIR = "ESC"
+const facturas = [];
+
+class Factura {
+    constructor(nombre, monto) {
+        this.nombre = nombre.toUpperCase();
+        this.monto = parseFloat(monto);
+        this.montoConIva = 0
+        this.items = []
+    }
+    sumaIva() {
+        this.montoConIva = this.monto * 1.21;
+    }
+    agregaDetalle(detalle) {
+        if ((detalle.nombre != undefined) && (Number(detalle.monto) != NaN)) {
+            this.items.push({
+                nombre: detalle.nombre,
+                monto: detalle.monto
+            })
+        }
+        return
+    }
+}
+
+for (let factura = 1; factura <= cantidad_facturas; factura++) {
+    let nombre = prompt(`ingrese nombre de factura o ${SALIR} para salir`)
+    if (escondicionSalida(nombre)) {
+        alert("GRACIAS POR SU SERVICIO")
+        break
+    }
+    
+    // let detalles = agregarDetalles()
+
+    let monto = Number(prompt("ingrese monto de factura"))
+    facturas.push(new Factura(nombre, monto));
+    total = calcularTotal(facturas)
+    console.log(`FACTURA ${factura} con nombre ${nombre} y monto ${monto}. El total de todo es ${total}`)
+}
+
+function agregarDetalles() {
+    for (let index = 0; index < 100; index++) {
+        let nombre = prompt(`ingrese el nombre del item o ${SALIR} para salir`)
+        let monto = prompt(`ingrese el monto del item o ${SALIR} para salir`)
+        agregaDetalle({nombre,monto})
+    }
+}
+
+function calcularTotal(items) {
+    let totalInterino = 0
+    for (const item of items) {
+        totalInterino = totalInterino + item.monto
+    }
+    return totalInterino
+}
+for (const factura of facturas)
+    factura.sumaIva();
+
+promedio = calcularPromedio(facturas)
+
+function escondicionSalida(texto) {
+    if (texto == SALIR) {
+        return true
+    }
+    return false
+}
+
+function calcularPromedio(items) {
+    let cantidad = items.length
+    let totalInterino = 0
+    for (const item of items) {
+        totalInterino = totalInterino + item.monto
+    }
+    console.log(`El promedio es ${totalInterino / cantidad}`)
+    return totalInterino / cantidad
+}
+//bonus track
+for (const factura of facturas) {
+    console.log(factura.monto)
+}
+
+let buscar = prompt("¿QUERES BUSCAR UNA FACTURA?, SI QUIERE CANCELAR INGRESE ESC")
+
+if (buscar != SALIR) {
+    const resultado = facturas.find((factura) => factura.nombre === buscar.toUpperCase())
+    alert(`FACTURA ${facturas.indexOf(resultado)+1} con nombre ${resultado.nombre} y monto ${resultado.monto}. La factura con IVA es ${resultado.montoConIva}. El total de todo es ${total}. El total con IVA es ${total*1.21}`)
+}
